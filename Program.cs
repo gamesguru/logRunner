238176722d84a24f441f4bd4450266d74a3a8d3c
gamesguru <mathmuncher11@gmail.com>
2018-01-05 11:49:34 -0500

    v0.01

--- Program.cs
+++ Program.cs
@@ -5,26 +5,27 @@ using System.Diagnostics;
 
 namespace logRunner
 {
-    class MainClass
+    class Program
     {
+        #region pub decs
         static string root = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
         static string sl = Path.DirectorySeparatorChar.ToString();
-        
-        
+
+
         public static class profile
         {
             public static string name;
             public static int index;
         }
-        
+
         public class _nutrient
         {
             public string field;
             public string rda;
             public string consumed;
         }
-       
-        
+
+
         static Dictionary<string, string[]> usdaPairs;
         static string usdaroot;
         static List<_nutrient> nutrients = new List<_nutrient>();
@@ -32,6 +33,7 @@ namespace logRunner
         static string[] activeFieldsLines;
         static string[] usdaNutKeyLines;
         static string outputLogFile;
+        #endregion
 
         public static void Main(string[] args)
         {
@@ -48,12 +50,13 @@ namespace logRunner
             np.rda = "27 mg";
             np.consumed = "11 mg";
             printp(np);
-            Console.ReadKey();
-            println("...fetching global keys...", ConsoleColor.DarkCyan);
+            //Console.ReadKey();
 
+            //updates user
+            println("...fetching global keys...", ConsoleColor.DarkCyan);
             if (args.Length == 0)
                 args = File.ReadAllLines($"{root}{sl}args.TXT");
-
+            
             //parses the arguments
             profile.index = Convert.ToInt32(args[0]);
             outputLogFile = args[1];
@@ -84,7 +87,7 @@ namespace logRunner
             List<string> fields = new List<string>();
             for (int i = 0; i < nutrients.Count; i++)
                 foreach (string s in usdaNutKeyLines)
-                    if (s.Split('|')[1] == nutrients[i].field)                    
+                    if (s.Split('|')[1] == nutrients[i].field)
                         fields.Add(nutrients[i].field);
 
             //reads in data from main database
@@ -94,21 +97,27 @@ namespace logRunner
                     if (st.Split('|')[1] == s)
                     {
                         string[] lines = File.ReadAllLines($"{usdaroot}{st.Split('|')[0]}");
-                        usdaPairs.Add(s, lines);                  
-                    }             
-
-            //prints the results            
+                        usdaPairs.Add(s, lines);
+                    }
             println();
+
+            //foodObj class??
+            //reads in the user data/foodlog
             for (int i = 2; i < args.Length; i++)
             {
                 dates.Add(args[i]);
-                printLog(args[i], nutrients);
+                //printLog(args[i], nutrients);
+            }
+            foreach (_nutrient n in nutrients){
+                
             }
 
+			//prints the results            
             println("press any key to exit...");
             Console.ReadKey();
         }
 
+        #region printp and printLog
         private static void printLog(string date, List<_nutrient> nuts)
         {
             println("==========", ConsoleColor.DarkCyan);
@@ -121,7 +130,7 @@ namespace logRunner
             foreach (string s in foodDayLines)
                 if (s.StartsWith("USDAstock"))
                     ;
-                    
+
             println(string.Join("\n", foodDayLines));
             //???
             println();
@@ -133,7 +142,7 @@ namespace logRunner
             double r = 1;
             try
             {
-				c = Convert.ToDouble(nut.consumed.Split(' ')[0]);
+                c = Convert.ToDouble(nut.consumed.Split(' ')[0]);
                 r = Convert.ToDouble(nut.rda.Split(' ')[0]);
             }
             catch (Exception e)
@@ -145,7 +154,7 @@ namespace logRunner
             ConsoleColor color = ConsoleColor.Blue;
             if (x > 1.0)
             {
-				x = 1.0;
+                x = 1.0;
                 color = ConsoleColor.DarkMagenta;
             }
             else if (x < 0.7 && x > 0.5)
@@ -166,9 +175,12 @@ namespace logRunner
             println(prog, color);
             return x;
         }
-        
+        #endregion
+
+        #region main print functions
         static List<string> outputLog = new List<string>();
-        private static void println(string s, ConsoleColor color = ConsoleColor.White){
+        private static void println(string s, ConsoleColor color = ConsoleColor.White)
+        {
             Console.ForegroundColor = color;
             Console.WriteLine(s);
             Console.ForegroundColor = ConsoleColor.White;
@@ -176,8 +188,9 @@ namespace logRunner
         }
 
         private static void println() => println("");
-        
-        private static void print(string s, ConsoleColor color = ConsoleColor.White){
+
+        private static void print(string s, ConsoleColor color = ConsoleColor.White)
+        {
             Console.ForegroundColor = color;
             Console.Write(s);
             Console.ForegroundColor = ConsoleColor.White;
@@ -192,5 +205,6 @@ namespace logRunner
             println($"\n\n{ ex.TargetSite}\n{ ex.StackTrace}");
             println(" ===================\n  --End exception--\n ===================", ConsoleColor.DarkRed);
         }
+        #endregion
     }
 }
